6ff7893167da6bcd43bead31600b03a6dcee58cf
LinWin-Cloud <2382546858@qq.com>
2023-09-16 08:52:39 -0400

    metaattacker.py

--- metaattacker.py
+++ metaattacker.py
@@ -1,8 +1,12 @@
 import os
 import sys
+import json
+
 
 if __name__ == "__main__":
     arg = sys.argv
     #print(len(arg))
     if len(arg) == 1:
-        print("")
+        import list_en
+        for i in list_en.list_of_name():
+            print(" - "+ i)
