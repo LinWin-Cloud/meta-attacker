0c4bfe861295d73a5f4996febf54702ddb25147e
LinWin-Cloud <2382546858@qq.com>
2023-09-16 09:24:07 -0400

    metaattacker.py

--- metaattacker.py
+++ metaattacker.py
@@ -1,12 +1,24 @@
 import os
 import sys
 import json
-
+import list_en
 
 if __name__ == "__main__":
     arg = sys.argv
     #print(len(arg))
     if len(arg) == 1:
-        import list_en
+
+        base_print = [
+            " -version        Show Version Information.",
+            "\n @ALL THE ATTACK CONTENT"
+        ]
+        for i in base_print:
+            print(i)
+
         for i in list_en.list_of_name():
-            print(" - "+ i)
+            print(" - "+ i + " ")
+            for j in list_en.get_options(i):
+                print("     [" + j + "] ")
+            print("     *For Example: \n          "+list_en.get_for_example(i))
+    else:
+        
