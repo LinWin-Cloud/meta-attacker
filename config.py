069a7801308b718bcbbf64d79683a268192bd22f
LinWin-Cloud <2382546858@qq.com>
2023-10-28 17:48:24 +0800

    delete

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-def get_language() -> str:
-    current_work_dir = os.path.dirname(__file__) 
-    f = open(current_work_dir + "/language.txt" , "r")
-    return f.read().replace("\n" , "")
--- http_server_attacks.py
+++
@@ -1,9 +0,0 @@
-import requests
-import threading
-
-def _attack(url , data) -> None:
-    print(str(url)+" "+str(data))
-
-def run(url: str , concurrent: int , data: int) -> None:
-    t = threading.Thread(target=_attack , args=(url , data))
-    t.start()
