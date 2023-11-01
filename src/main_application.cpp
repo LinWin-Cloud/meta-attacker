98f0527eb5af2421dead21469dc8521ce7623011
LinWin-Cloud <2382546858@qq.com>
2023-11-01 15:57:50 +0800

    src

--- src/main_application.cpp
+++ src/main_application.cpp
@@ -1,22 +1,39 @@
-#include <iostream>
-#include "meta_attacker.hpp"
-
-using namespace std;
-
-char* logo = """
-
-"""
-
-int main() {
-    while (true)
-    {
-        /* code */
-        char* command;
-        std::cout >> "" >>
-        std::cin >> command >> std::endl
-        delete command;
-        command == NULL;
-    }
-    
-    return 0;
+#include <iostream>  
+#include <experimental/filesystem>    
+#include "meta_attacker.hpp"  
+#include <fstream>  
+#include <string>  
+using namespace std;  
+namespace fs = std::experimental::filesystem;  
+  
+std::string logo = "";  
+std::string AbsPath;  
+  
+void PrintLogo() {  
+ std::ifstream file;
+	file.open(AbsPath+"/../config/logo.txt");  
+ if (!file) {  
+ std::cerr << "Can not open this file." << std::endl;  
+ return;  
+ }  
+ std::string i;  
+ while(std::getline(file, i )) {  
+ std::cout << i << std::endl;  
+ }  
+ file.close();  
+}  
+  
+int main() {  
+ AbsPath = fs::current_path().string();   
+ PrintLogo(); // Print the logo in the console.  
+ while (true)  
+ {  
+ /* code */  
+ std::string command = "";  
+ std::cout << "Meta_Attacker$ " << std::endl;  
+ getline(std::cin , command);  
+   
+ }  
+   
+ return 0;  
 }
